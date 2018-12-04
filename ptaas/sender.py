1320628df45e37060c74c9a208d8dd40db958613
David Greve <david.greve@rockwellcollins.com>
2018-12-04 13:25:52 -0600

    sender/relay

---
+++ ptaas/sender.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+from ctypes import *
+import sys
+import argparse
+import socket
+
+###############################################################################
+
+class c_tuple(Structure):
+    _fields_ = [("x" , c_int),
+                ("y" , c_int),
+                ("z" , c_int)
+            ]
+
+class c_rgb(Structure):
+    _fields_ = [("r" , c_int),
+                ("g" , c_int),
+                ("b" , c_int)
+            ]
+
+class c_msg(Structure):
+    _fields_ = [("object"   , c_int),
+                ("spec"     , c_int),
+                ("parm"     , c_int),
+                ("position" , c_tuple),
+                ("vector"   , c_tuple),
+                ("color"    , c_rgb),
+                ("emission" , c_rgb)
+            ]
+
+_libptass = CDLL('libptaas.so')
+_libptass.step.restype  = c_msg
+
+def cstep():
+    global _libptass
+    return _libptass.step()
+
+###############################################################################
+
+class SENDER():
+
+    def __init__(self,relay_address):
+        self.relay_address = relay_address
+
+    def openSockets(self):
+        self.wsock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.wsock.connect(self.relay_address)
+
+    def closeSockets(self):
+        self.wsock.close()
+
+    def run(self):
+        self.openSockets()
+        try:
+            while True:
+                msg = self.getTestVector()
+                self.processTestVector(msg)
+        finally:
+            self.closeSockets()
+
+    def getTestVector(self):
+        return cstep()
+
+    def processTestVector(self,struc):
+        msg = ""
+        msg += ' object {}'.format(struc.object)
+        msg += ' spec {}'.format(struc.spec)
+        msg += ' parm {}'.format(struc.parm)
+        msg += ' position.x {}'.format(struc.position.x)
+        msg += ' position.y {}'.format(struc.position.y)
+        msg += ' position.z {}'.format(struc.position.z)
+        msg += ' vector.x {}'.format(struc.vector.x)
+        msg += ' vector.y {}'.format(struc.vector.y)
+        msg += ' vector.z {}'.format(struc.vector.z)
+        msg += ' color.r {}'.format(struc.color.r)
+        msg += ' color.g {}'.format(struc.color.g)
+        msg += ' color.b {}'.format(struc.color.b)
+        msg += ' emission.r {}'.format(struc.emission.r)
+        msg += ' emission.g {}'.format(struc.emission.g)
+        msg += ' emission.b {}'.format(struc.emission.b)
+        print('sending : ' + msg)
+        self.wsock.send(bytes(msg, 'utf-8'))
+
+###############################################################################
+def main():
+    parser = argparse.ArgumentParser(description="PTAAS Relay")
+    parser.add_argument('-r', '--relay',
+                        required=False,
+                        help="The URL of the Relay [localhost]")
+    parser.set_defaults(fuzzer='localhost')
+    args = parser.parse_args()
+    relay_port = 9973
+    sender = SENDER((args.fuzzer,relay_port))
+    sender.run()
+
+###############################################################################
+if __name__ == "__main__":
+    sys.exit(main())
---
+++ ptaas/wrapper.c
@@ -0,0 +1,54 @@
+#include "ptaas.h"
+#include <stdlib.h>
+
+typedef struct tuple {
+  int x;
+  int y;
+  int z;  
+} tuple_t;
+
+typedef struct rgb {
+  int r;
+  int g;
+  int b;
+} rgb_t;
+
+typedef struct msg {
+  int     object;
+  int     spec;
+  int     parm;
+  tuple_t position;
+  tuple_t vector;
+  rgb_t   color;
+  rgb_t   emission;
+} msg_t;
+
+extern int generateRandomValue(_Bool lflag, _Bool uflag, int lbound, int ubound) {
+  int min = lflag ? lbound : lbound+1;
+  int max = uflag ? ubound : ubound-1;
+  int range = max - min + 1;
+  double rnd = ((double) rand())/(1.0 + ((double) RAND_MAX));
+  int value = ((int) (((double) range)*rnd));
+  return value + min;
+}
+
+msg_t step() {
+  updateFunction();
+  msg_t msg;
+  msg.object      = object[0];
+  msg.spec        = spec[0];
+  msg.parm        = parm[0];
+  msg.position.x  = position_x[0];
+  msg.position.y  = position_y[0];
+  msg.position.z  = position_z[0];
+  msg.vector.x    = vector_x[0];
+  msg.vector.y    = vector_y[0];
+  msg.vector.z    = vector_z[0];
+  msg.color.r     = color_r[0];
+  msg.color.g     = color_g[0];
+  msg.color.b     = color_b[0];
+  msg.emission.r  = emission_r[0];;
+  msg.emission.g  = emission_g[0];;
+  msg.emission.b  = emission_b[0];;
+  return msg;
+}
