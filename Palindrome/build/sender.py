9e41dcd29f438ddb459c0e27da23b000cef8865d
Jeffrey Chen <jeffrey.chen.mn@gmail.com>
2019-08-26 21:13:20 +0000

    Upload New File

---
+++ Palindrome/build/sender.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+from ctypes import *
+import sys
+import argparse
+import base64
+import time
+
+###############################################################################
+class c_msg(Structure):
+    _fields_ = [("length" , c_int),
+                ("chas"   , c_int * 200)
+            ]
+
+_lib_Palindrome = CDLL('../lib/lib_Palindrome.so')
+_lib_Palindrome.step.restype  = c_msg
+
+def cstep():
+    global _lib_Palindrome
+    return _lib_Palindrome.step()
+
+###############################################################################
+class SENDER():
+    def __init__(self):
+        pass
+
+    def run(self):
+        while True:
+            msg = self.getTestVector()
+            self.processTestVector(msg)
+            time.sleep(0.1)
+
+    def getTestVector(self):
+        return cstep()
+
+    def processTestVector(self,struc):
+        testStr = ""
+        for num in struc.chas:
+            asc = chr(int(num) % 95 + 32)
+            testStr += asc
+
+        msg = ""
+        msg += " length {}".format(struc.length)
+        msg += " testStr {}".format(testStr)
+
+        e64 = base64.b64encode(msg.encode()).decode()
+        sys.stdout.write(e64 + '\n')
+        sys.stdout.flush()
+
+###############################################################################
+def main():
+    parser = argparse.ArgumentParser(description="Palindrome Relay")
+    sender = SENDER()
+    sender.run()
+
+###############################################################################
+if __name__ == "__main__":
+    sys.exit(main())
