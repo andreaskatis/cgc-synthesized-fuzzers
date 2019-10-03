3184f2f9cb9378abb3431f09cb8249734ea976f6
jchen677 <Jeffrey.Chen.MN@GMail.com>
2019-10-02 20:47:04 -0700

    Add basic data gathering functionality

--- scripts/autoFuzz.py
+++ scripts/autoFuzz.py
@@ -7,6 +7,23 @@ import sys
 from subprocess import Popen, PIPE
 HOST = '0.0.0.0' #The server's hostname or IP address
 
+def parseCoverage(coverage, result):
+	executed = 0
+	total = 0
+	srcLine = 0
+	while True:
+		if (srcLine == -1):
+			break
+		srcLine = coverage.find("src", srcLine)
+		execStart = coverage.find("\"lines_executed\":")
+		result.write(str(execStart) + "/" + coverage[execStart] + " ")
+
+	result.write("\n")
+	for i in range(0, len(coverage)):
+		if (coverage[i] == "{"):
+			result.write("\n")
+		result.write(coverage[i])
+
 def main(argv):
 	benchmark = argv
 	benchDir = "../" + benchmark
@@ -33,34 +50,29 @@ def main(argv):
 	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	sock.connect((HOST, PORT))
 	debug = open("debug_run.txt", "a")
-	debug.write("Benchmark: {bench}\nPORT: {port}\nSID: {sid}\n".format(bench = benchmark, port = PORT, sid = SID))
-	servConnect = "localhost:{port}/session/{sid}".format(port = PORT, sid = SID)
+	debug.write("Benchmark: {bench}\nPORT: {port}\nSID: {sid}\n\n".format(bench = benchmark, port = PORT, sid = SID))
+	servConnect = "localhost:5000/session/{sid}".format(port = PORT, sid = SID)
 
 	#Run fuzzer
+	data = open("results.txt", "a")
 	while True:
 		# proc = subprocess.check_output(["./fuzz.sh"])
 		proc = subprocess.Popen(["./fuzz.sh"], stdout = PIPE, stderr = PIPE)
 		stdout, stderr = proc.communicate()
-		# debug.write("\n")
 		sock.sendall(stdout)
 
-		# Collect data and print to results		
-		# data = open("results.txt", "a")
-		# cvg = subprocess.Popen(["curl", "-X", "GET", servConnect + "/coverage"], stdout = PIPE, stderr = PIPE)
-		# stdout, stderr = cvg.communicate()
-		# # coverage = subprocess.check_output(["curl", "-X", "GET", servConnect + "/coverage"])
-		# print(stdout)
-		# debug.write(stdout.decode('utf-8'))
-		# data.write(stdout.decode('utf-8'))
-		# data.write("\n")
-		# debug.write("\n")	
+		# Collect data and print to results
+		coverage = subprocess.check_output(["curl", "-X", "GET", servConnect + "/coverage"])
+		data.write(coverage)
+		data.write("\n")	
 
-		# events = subprocess.check_output(["curl", "-X", "GET", servConnect + "/events"])
-		# debug.write({}.format(events))
-		# results.write({}.format(events))
-		# results.write("\n\n")
-		# debug.write("\n")
-		# data.close()
+		events = subprocess.check_output(["curl", "-X", "GET", servConnect + "/events"])
+		data.write(coverage)
+		data.write("\n\n")
+		break #Temporary measure, force while to run once to simplify debugging
+
+	result = open("temp.txt", "w")
+	parseCoverage(coverage, result)
 
 if (__name__ == "__main__"):
 	main(sys.argv[1])
